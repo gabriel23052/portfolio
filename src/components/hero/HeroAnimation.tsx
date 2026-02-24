a2977ef8806bbb8b092161105636851e418d3eac
Gabriel <gabrielggp2002@gmail.com>
2026-02-24 17:29:20 -0300

    Animação no Hero
    - Implementa a animação no Hero, já inclui o estilo, a responsividade e a acessibilidade

--- src/components/hero/HeroAnimation.tsx
+++ src/components/hero/HeroAnimation.tsx
@@ -1,12 +1,34 @@
 import classes from "./HeroAnimation.module.css";
 
 /**
- * Animação que fará parte do Hero futuramente
+ * Animação do Hero
  */
 const HeroAnimation = () => {
   return (
     <div className={classes.container} aria-hidden="true">
-      TODO
+      <div className={classes.animation}>
+        <div className={classes.macControls}>
+          <div data-color="green"></div>
+          <div data-color="yellow"></div>
+          <div data-color="red"></div>
+        </div>
+        <div className={classes.grid}>
+          <div className={classes.command}>
+            <span>build --ui --code</span>
+          </div>
+          <div className={classes.uiElements}>
+            <div data-index="0"></div>
+            <div data-index="1"></div>
+            <div data-index="2"></div>
+            <div data-index="3"></div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.shadow}></div>
+      <div className={classes.background}>
+        <div className={classes.primaryCircle}></div>
+        <div className={classes.secondaryCircle}></div>
+      </div>
     </div>
   );
 };
